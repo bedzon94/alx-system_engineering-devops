e1e4a5b5bc9817e1b227d8227c675903490c6b21
Norris <99494922+bedzon94@users.noreply.github.com>
2022-10-17 18:38:09 -0400

    3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -21,12 +21,7 @@ def make_all(users=None, todos=None):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2 and sys.argv[1].isdigit():
-        args_id = {"id": sys.argv[1]}
-        users = requests.get("https://jsonplaceholder.typicode.com/users",
-                             params=args_id).json()
-        args_userid = {"userId": sys.argv[1]}
-        todos = requests.get("https://jsonplaceholder.typicode.com/todos",
-                             params=args_userid).json()
+        users = requests.get("https://jsonplaceholder.typicode.com/users").json()
+        todos = requests.get("https://jsonplaceholder.typicode.com/todos").json()
 
     make_all(users, todos)
