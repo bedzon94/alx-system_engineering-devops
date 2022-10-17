f70891e41e9cda4ae15c44d59d101c0663fd44f6
Norris <99494922+bedzon94@users.noreply.github.com>
2022-10-17 18:57:43 -0400

    3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -13,7 +13,7 @@ def make_all(users=None, todos=None):
             u = i.get("id")
             for i in todos:
                 if u == i.get("userId"):
-                    all_list.append({"username": users[0].get("username"),
+                    all_list.append({"username": users.get("username"),
                                      "task": i.get("title"),
                                      "completed": i.get("completed")})
             alljson[u] = all_list
